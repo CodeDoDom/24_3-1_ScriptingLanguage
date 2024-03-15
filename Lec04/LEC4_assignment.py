f52a94bf69a14181a169d57cf9c71b0d3e8e4930
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-03-15 17:04:14 +0900

    과제#3 작성 중

--- Lec04/LEC4_assignment.py
+++ Lec04/LEC4_assignment.py
@@ -1,21 +1,24 @@
-def tmp_merge(*word):
-    sen = ''
-    cnt = len(word)     # 가변 인자의 개수
+def merge(*word):
+    
 
-    for i in word:
-        if cnt == 1:
-            sen += 'and '
-        sen += i
-        cnt = cnt-1
-        if cnt > 0:
-            sen += ','
+final = merge('orange', 'apple', 'mango', 'banana', 'peanut')
+final
+type(final)
 
-    print(f'문장: {sen}')
-    print(f'타입: {type(sen)}')
-
-
-tmp_merge('orange', 'apple', 'mango', 'banana', 'peanut')
-
-#final = merge('orange', 'apple', 'mango', 'banana', 'peanut')
-#final
-#type(final)
+# def tmp_merge(*word):
+#     sen = ''
+#     cnt = len(word)     # 가변 인자의 개수
+#
+#     for i in word:
+#         if cnt == 1:
+#             sen += 'and '
+#         sen += i
+#         cnt = cnt-1
+#         if cnt > 0:
+#             sen += ','
+#
+#     print(f'문장: {sen}')
+#     print(f'타입: {type(sen)}')
+#
+#
+# tmp_merge('orange', 'apple', 'mango', 'banana', 'peanut')
