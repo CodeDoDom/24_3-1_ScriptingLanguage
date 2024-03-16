43d768d481ae7a381c62051eec1a060ec91702bd
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-03-16 13:13:31 +0900

    과제#3_리스트실습과제 완료
    미제출

--- Lec04/LEC4_assignment.py
+++ Lec04/LEC4_assignment.py
@@ -1,24 +1,37 @@
 def merge(*word):
+    sen = ''
+    cnt = len(word)     # 가변 인자의 개수
+
+    for i in word:
+        if cnt == 1:
+            sen += 'and '
+        sen += i
+        cnt = cnt-1
+        if cnt > 0:
+            sen += ','
+
+    return sen
     
 
 final = merge('orange', 'apple', 'mango', 'banana', 'peanut')
-final
-type(final)
+print(final)
+print(type(final))
 
-# def tmp_merge(*word):
-#     sen = ''
-#     cnt = len(word)     # 가변 인자의 개수
-#
-#     for i in word:
-#         if cnt == 1:
-#             sen += 'and '
-#         sen += i
-#         cnt = cnt-1
-#         if cnt > 0:
-#             sen += ','
-#
-#     print(f'문장: {sen}')
-#     print(f'타입: {type(sen)}')
-#
-#
-# tmp_merge('orange', 'apple', 'mango', 'banana', 'peanut')
+
+# copilot
+def c_merge(*args):
+    # Join all input strings with commas
+    result = ', '.join(args)
+
+    # Insert 'and' before the last string
+    last_comma_index = result.rfind(',')
+    if last_comma_index != -1:
+        result = result[:last_comma_index] + ' and' + result[last_comma_index + 1:]
+
+    return result
+
+
+# Example usage
+final = c_merge('orange', 'apple', 'mango', 'banana', 'peanut')
+print(final)  # Output: 'orange, apple, mango, banana, and peanut'
+print(type(final))  # Output: <class 'str'>
