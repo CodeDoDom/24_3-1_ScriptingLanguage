2f8fe4ae9d90efffea25968934bad64b716a41c7
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-03-22 18:22:21 +0900

    HW5_해결 중
    문자열을 공백 기준으로 분리하는 split_sentence(sen) 함수 추가.

--- LEC06/2022182024_HW5.py
+++ LEC06/2022182024_HW5.py
@@ -2,15 +2,23 @@ import keyboard
 import pyperclip
 
 
-def split_sentence(sen):   # 문자열 str을 공백 기준으로 분리
-    print(f'{sen}')
-
-
-def copy_clipboard_to_sen():    # 클립보드에 저장된 내용을 str에 저장함
+def copy_clipboard_to_sen():    # 클립보드에 저장된 내용을 sen에 저장함
     global sen
     sen = pyperclip.paste()
     # keyboard.write(sen)
     split_sentence(sen)
+    count_words(words)
+
+
+def split_sentence(sen):   # 문자열 sen을 공백 기준으로 분리
+    global words
+    print(f'원본 문장: {sen}')
+    words = sen.split()
+    print(f'분리: {words}')
+
+
+def count_words(words):
+    pass
 
 
 keyboard.add_hotkey('shift+windows+w', copy_clipboard_to_sen)
