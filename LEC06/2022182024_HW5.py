f954bd1b5cc3e44787bd36224de76f9aee65ee56
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-03-22 22:56:55 +0900

    HW5_count_words(words) 함수 작성
    단어의 개수를 세는 count_words() 함수 작성

--- LEC06/2022182024_HW5.py
+++ LEC06/2022182024_HW5.py
@@ -5,9 +5,9 @@ import pyperclip
 def copy_clipboard_to_sen():    # 클립보드에 저장된 내용을 sen에 저장함
     global sen
     sen = pyperclip.paste()
-    # keyboard.write(sen)
     split_sentence(sen)
     count_words(words)
+    print_cnt_words(cnt_word, 20, 20)
 
 
 def split_sentence(sen):   # 문자열 sen을 공백 기준으로 분리
@@ -18,7 +18,18 @@ def split_sentence(sen):   # 문자열 sen을 공백 기준으로 분리
 
 
 def count_words(words):
-    pass
+    global cnt_word
+    cnt_word = {}
+    for word in words:
+        if word.isalpha():  # 단어가 알파벳으로만 이루어져있다면
+            cnt_word[word] = cnt_word.get(word, 0) + 1
+    return cnt_word
+
+
+def print_cnt_words(cnt_word, lwidth, rwidth):
+    print('Word Count'.center(lwidth + rwidth, '='))
+    for k, v in cnt_word.items():
+        print(k.ljust(lwidth, ' ') + str(v).rjust(rwidth))
 
 
 keyboard.add_hotkey('shift+windows+w', copy_clipboard_to_sen)
